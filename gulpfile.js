590a4b2808ca7e69aca10df98d79ce0ea0aa51b6
Artem Goncharov <skymen@inuits.eu>
2017-03-09 15:32:34 +0200

    New start

--- gulpfile.js
+++ gulpfile.js
@@ -1,131 +1,126 @@
-'use strict';
 
-var gulp = require('gulp'),
-    watch = require('gulp-watch'), // Ребилд только измененных файлов
-    plumber = require('gulp-plumber'), // Защита gulp от вылета
-    uglify = require('gulp-uglify'),
-    sass = require('gulp-sass'),
-    sourcemaps = require('gulp-sourcemaps'),
-    rimraf = require('rimraf'), // Очистка директорий
-    twig = require('gulp-twig'), // JS шаблонизатор с синтаксисом TWIG
-    browserSync = require("browser-sync"),
-    kss = require('gulp-kss'),
-    reload = browserSync.reload;
+// Imports
 
-var path = {
-    build: {
-        html: '/',
-        js: 'js/',
-        css: 'css/',
-        cssimg: 'css/img/',
-        images: 'images/',
-        fonts: 'fonts/',
-    },
-    src: {
-        html: 'src/*.html',
-        js: 'src/js/*.js',
-        jsmain: 'src/js/main.js',
-        scss: 'src/scss/*.*',
-        cssimg: 'src/scss/img/**/*.*',
-        images: 'src/images/**/*.*',
-        fonts: 'src/fonts/**/*.*',
-    },
-    watch: {
-        html: 'src/**/*.html',
-        js: 'src/js/**/*.js',
-        scss: 'src/scss/**/*.scss',
-        cssimg: 'src/scss/img/**/*.*',
-        images: 'src/images/**/*.*',
-        fonts: 'src/fonts/**/*.*'
-    },
-    clean: './build'
-};
+var gulp = require('gulp'); 
+var sass = require('gulp-sass');
+var styleguide = require('sc5-styleguide');
 
+// Path definitions
 
-var config = {
-    server: {
-        baseDir: "./build"
-    },
-    tunnel: true,
-    host: 'localhost',
-    port: 9000,
-    logPrefix: "Frontend_Devil"
-};
+var sourcePath = 'src';
+var htmlWild = sourcePath + '/**/*.html';
+var styleSourcePath = sourcePath + '/styles';
+var scssWild = styleSourcePath + '/**/*.scss';
+var scssRoot = styleSourcePath + '/tutorial.scss';
+var overviewPath = styleSourcePath + '/README.md';
 
+var buildPath = 'build';
+var styleBuildPath = buildPath + '/styles';
+var styleguideAppRoot = '/';
+var styleguideBuildPath = buildPath + styleguideAppRoot;
 
+var tmpPath = '/var/www/swc';
+var styleguideTmpPath = tmpPath + '/styleguide';
 
-gulp.task('webserver', function () {
-   // browserSync(config); // Запуск веб-сервера
+// Building the application
+//
+// In reality the app would ofcourse be a lot more complex.
+// Here the app simply consists of some HTML so we get to examine how
+// the styles would be used in the application. A key relevation is
+// that the markup needs to be written into the app. There is no magic
+// that would bring the markup for a page into the app from the pages
+// section in the styleguide.
+
+gulp.task('html', function() {
+    return gulp.src(htmlWild)
+        .pipe(gulp.dest(buildPath));
 });
 
-gulp.task('clean', function (cb) {
-    rimraf(path.clean, cb);
+gulp.task('scss', function() {
+    return gulp.src(scssRoot)
+        .pipe(sass())
+        .pipe(gulp.dest(styleBuildPath));
 });
 
-gulp.task('html:build', function () {
-    gulp.src(path.src.html) 
-        .pipe(plumber())
-        .pipe(gulp.dest(path.build.html))
-        .pipe(reload({stream: true}));
+// Building styleguide for static hosting to be displayed as a part of the application
+//
+// Here we build the styleguide so it can be included in a web folder within the app.
+// The benefit for including the styleguide at /styleguide path of the app is that
+// everyone can always find a master copy of the style guide. Another benefit is that
+// this copy will be load balanced by the web server, so it can handle heavy loads.
+// All interactive features are disabled to prevent users from tampering with the
+// styles.
+
+gulp.task('staticStyleguide:generate', function() {
+  return gulp.src(scssWild)
+    .pipe(styleguide.generate({
+        title: 'Sky Components',
+        rootPath: styleguideBuildPath,
+        appRoot: styleguideAppRoot,
+        overviewPath: overviewPath
+      }))
+    .pipe(gulp.dest(styleguideBuildPath));
 });
 
-gulp.task('js:build', function () {
-    gulp.src(path.src.js) 
-        .pipe(plumber())
-        .pipe(sourcemaps.init())
-        .pipe(sourcemaps.write('/')) 
-        .pipe(gulp.dest(path.build.js))
-        .pipe(reload({stream: true}));
+gulp.task('staticStyleguide:applystyles', function() {
+  return gulp.src(scssRoot)
+    .pipe(sass({
+      errLogToConsole: true
+    }))
+    .pipe(styleguide.applyStyles())
+    .pipe(gulp.dest(styleguideBuildPath));
 });
 
-gulp.task('style:build', function () {
-    gulp.src(path.src.scss) 
-        .pipe(plumber())
-        .pipe(sourcemaps.init())
-        .pipe(sass()).on('error', sass.logError)
-        .pipe(sourcemaps.write('/'))
-        .pipe(gulp.dest(path.build.css))
-        .pipe(reload({stream: true}));
+gulp.task('staticStyleguide', ['staticStyleguide:generate', 'staticStyleguide:applystyles']);
+
+// Running styleguide development server to view the styles while you are working on them
+//
+// This task runs the interactive style guide for use by developers. It runs a built-in server
+// and contains all the interactive features and should be updated automatically whenever the
+// styles are modified.
+
+gulp.task('styleguide:generate', function() {
+  return gulp.src(scssWild)
+    .pipe(styleguide.generate({
+        title: 'Sky Components',
+      //  server: true,
+        rootPath: styleguideTmpPath,
+        overviewPath: overviewPath
+      }))
+    .pipe(gulp.dest(styleguideTmpPath));
 });
 
-
-gulp.task('fonts:build', function() {
-    gulp.src(path.src.fonts)
-        .pipe(plumber())
-        .pipe(gulp.dest(path.build.fonts))
+gulp.task('styleguide:applystyles', function() {
+  return gulp.src(scssRoot)
+    .pipe(sass({
+      errLogToConsole: true
+    }))
+    .pipe(styleguide.applyStyles())
+    .pipe(gulp.dest(styleguideTmpPath));
 });
 
+gulp.task('styleguide', ['styleguide:generate', 'styleguide:applystyles']);
 
-gulp.task('kss', function () {
-    gulp.src(path.src.scss)
-        .pipe(kss({
-            overview: 'kss/styleguide.md',
-            templateDirectory: 'kss/template'
-        }))
-        .pipe(gulp.dest('kss/'));
+// Developer mode
+
+gulp.task('dev', ['html', 'scss', 'styleguide'], function() {
+    gulp.watch(htmlWild, ['html']);
+    gulp.watch(scssWild, ['scss', 'styleguide']);
+    console.log(
+        '\nDeveloper mode!\n\nSC5 Styleguide available at http://localhost:3000/\n'
+    );
 });
 
-gulp.task('build', [
-    'html:build',
-    'js:build',
-    'style:build',
-    'fonts:build',
-]);
+// The basic build task
 
-gulp.task('watch', function(){
-    watch([path.watch.html], function(event, cb) {
-        gulp.start('html:build');
-    });
-    watch([path.watch.scss], function(event, cb) {
-        gulp.start('style:build');
-    });
-    watch([path.watch.js], function(event, cb) {
-        gulp.start('js:build');
-    });
-    watch([path.watch.fonts], function(event, cb) {
-        gulp.start('fonts:build');
-    });
+gulp.task('default', ['html', 'scss', 'staticStyleguide'], function() {
+    console.log(
+        '\nBuild complete!\n\nFresh build available in directory: ' +
+        buildPath + '\n\nCheckout the build by commanding\n' +
+        '(cd ' + buildPath + '; python -m SimpleHTTPServer)\n' +
+        'and pointing yout browser at http://localhost:8000/\n' +
+        'or http://localhost:8000/styleguide/ for the styleguide\n\n' +
+        'Run gulp with "gulp dev" for developer mode and style guide!\n'
+    );
 });
 
-
-gulp.task('default', ['build', 'webserver', 'watch']);
